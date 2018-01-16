1e5fea49aef734e261c6ef5d4d48a538dc9973ad
zouhao <zouh@ducetech.com>
2018-01-16 19:09:05 +0800

    bug

--- src/main/java/com/sagacn/fugathering/service/TokenService.java
+++ src/main/java/com/sagacn/fugathering/service/TokenService.java
@@ -26,37 +26,4 @@ public class TokenService {
         return token;
     }
 
-    public String getNextPicture(String token,String qrNumber){
-        if(redis.sismember(CacheKeyConstant.QR_PREFIX+token,qrNumber)){
-            throw new BaseException(ErrorCode.URL_ALREADY_EXIST);
-        }
-        List<String> total = redis.lrange(CacheKeyConstant.TOTAL_SET, 0, -1);
-        Set<String> userPics = redis.smembers(token);
-        if (userPics!=null){
-            total.removeAll(userPics);
-        }
-        int i = RandomUtils.nextInt(0, total.size());
-        redis.sadd(token,total.get(i));
-        redis.sadd(CacheKeyConstant.QR_PREFIX+token,qrNumber);
-        return total.get(i);
-    }
-
-    public List<String> getUserPictures(String token){
-        if (!redis.exists(token)){
-            return new ArrayList<>();
-        }
-        Set<String> userPics = redis.smembers(token);
-        List<String> result=new ArrayList<>(userPics);
-        Collections.sort(result);
-        return result;
-    }
-
-    public Long hasDrawn(String token){
-        Double zscore = redis.zscore(CacheKeyConstant.DRAWN_TIME_KEY,token);
-        if (zscore==null){
-            redis.zadd(CacheKeyConstant.DRAWN_TIME_KEY, new Double(System.currentTimeMillis()),token);
-            return null;
-        }
-        return zscore.longValue();
-    }
 }
